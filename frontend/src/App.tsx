0288eeb29884ecff7f35922f90eac6117c3388f4
Dariusz Luśnia <dariusz.lusnia@gmail.com>
2024-09-14 11:36:38 +0200

    Add initial react ts tailwind project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import wailsLogo from "./assets/wails.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white grid grid-cols-1 place-items-center justify-items-center mx-auto py-8">
+      <div className="text-blue-900 text-2xl font-bold font-mono">
+        <h1 className="content-center">Vite + React + TS + Tailwind</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
